7444ed7df5b3962ec32a13d3fca0e822d86b96c6
kenilshahh <kenilshah1505@gmail.com>
2023-04-09 01:21:21 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,37 +1,71 @@
 /** @format */
-
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 import React from 'react';
 import Landing from './components/landing';
 import Feature from './components/feature';
 import Team from './components/team';
 import Elite from './components/Elite';
-
+import { useSmartAccountContext } from './Biconomy/SmartAccountContext.tsx';
+import { useWeb3AuthContext } from './Biconomy/SocialLoginContext.tsx';
+import ConnectButton from './Connect.tsx';
+import './App.css';
+import { PolybaseProvider } from '@polybase/react';
+import { Polybase } from '@polybase/client';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Logo from './components/logo';
 
+const polybase = new Polybase();
 function App() {
+  const {
+    address,
+    loading: eoaLoading,
+    userInfo,
+    connect,
+    disconnect,
+    getUserInfo,
+    web3Provider,
+  } = useWeb3AuthContext();
+  const {
+    selectedAccount,
+    loading: scwLoading,
+    setSelectedAccount,
+    balance: allTokensBalance,
+    // wallet
+  } = useSmartAccountContext();
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<Landing logo={Logo} />}
-        />
-        <Route
-          path="/feature"
-          element={<Landing logo={Elite} />}
-        />
-        <Route
-          path="/team"
-          element={<Team />}
-        />
-        <Route
-          path="/elite"
-          element={<Elite />}
-        />
-      </Routes>
-    </Router>
-
+    <>
+      <PolybaseProvider polybase={polybase}>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={<Landing logo={Logo} />}
+            />
+            <Route
+              path="/feature"
+              element={<Landing logo={Elite} />}
+            />
+            <Route
+              path="/team"
+              element={<Team />}
+            />
+            <Route
+              path="/elite"
+              element={<Elite />}
+            />
+          </Routes>
+        </Router>
+      </PolybaseProvider>
+    </>
     // <div>
     //   <Landing />
     //   <Feature />
