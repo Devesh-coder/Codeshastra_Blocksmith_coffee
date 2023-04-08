8c7706c778fb96770c758e3a3adf5cbfa0f60241
kenilshahh <kenilshah1505@gmail.com>
2023-04-08 12:24:36 +0530

    front end done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Landing from './components/landing'
+import Feature from './components/feature'
+
+function App() {
+  return (
+    <div>
+      <Landing />
+      <Feature/>
+    </div>
+  )
+}
+
+export default App
