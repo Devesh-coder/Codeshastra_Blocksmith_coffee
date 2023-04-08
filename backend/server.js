c53574dd7f2ea2bb7075aabb6d58337fb4c66402
Devesh Agarwal <agarwaldevesh326@gmail.com>
2023-04-08 11:31:14 +0530

    Server and frontend connected

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 app.get('/', (req, res) => {
 	console.log('Hello World!')
-	res.send('Hello World!')
+	res.send('Server on and working')
 })
 
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import './App.css'
+import axios, * as others from 'axios'
+
+import { useEffect, useState } from 'react'
 
 function App() {
-	return <div>Working</div>
+	const [data, setData] = useState()
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = axios.get('http://localhost:8000/')
+			const { data } = await result
+			setData(data)
+			console.log('working')
+		}
+		fetchData()
+	}, [])
+	return <div>Working with {data}</div>
 }
 
 export default App
