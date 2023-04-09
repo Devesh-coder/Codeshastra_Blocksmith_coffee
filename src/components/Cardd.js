ed7c95c3c3e12f18640d775f72d6e3d85172ee74
kenilshahh <kenilshah1505@gmail.com>
2023-04-09 13:41:50 +0530

    done final

--- src/components/Cardd.js
+++ src/components/Cardd.js
@@ -4,7 +4,9 @@ import React from 'react';
 import card from '../assets/Screenshot 2023-04-09 090611.png';
 export default function Cardd() {
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div
+      class="max-w-sm rounded overflow-hidden shadow-lg"
+      style={{ borderRadius: '30px' }}>
       <img
         class="w-full"
         src={card}
@@ -13,9 +15,16 @@ export default function Cardd() {
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">Ettarra Gold Membership NFT</div>
         <div class="text-gray-700 text-base">Free access to all events</div>
+        <div class="text-gray-700 text-base">
+          Flat 30% off for All the Products
+        </div>
+        <div class="text-gray-700 text-base">
+          Get any Cold Coffee (450ml) for free (worth upto ₹450/-),
+        </div>
+        <div class="text-gray-700 text-base">Official member t-shirt</div>
       </div>
       <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        {/* <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
           #photography
         </span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
@@ -23,7 +32,7 @@ export default function Cardd() {
         </span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
           #winter
-        </span>
+        </span> */}
       </div>
     </div>
   );
