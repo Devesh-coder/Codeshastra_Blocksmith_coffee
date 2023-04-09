aa899308a4c09cc9b623637ebd92dda3ce0f9657
arshnirmal <arshnirmal11@gmail.com>
2023-04-09 10:47:54 +0530

    final

--- frontend/src/components/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import CoffeeContext from '../context/CoffeContext'
-import { useContext } from 'react'
-
-function HomePage() {
-	const { data } = useContext(CoffeeContext)
-
-	return <div>HomePage and {data}</div>
-}
-export default HomePage
